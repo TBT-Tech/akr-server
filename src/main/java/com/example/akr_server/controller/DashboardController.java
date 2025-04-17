f1a2576acb535a3134834e6f753d8933e22235e1
Developer <33284585+AmarnathAyyananChakkaravarthy@users.noreply.github.com>
2025-04-17 12:57:02 +0530

    Remove Jackson JSON Dependency | Amarnath

--- src/main/java/com/example/akr_server/controller/DashboardController.java
+++ src/main/java/com/example/akr_server/controller/DashboardController.java
@@ -1,11 +1,7 @@
 package com.example.akr_server.controller;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
